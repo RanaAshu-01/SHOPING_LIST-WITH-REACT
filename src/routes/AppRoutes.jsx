07ca4bb415ececf2e3a97916eeefb154090106ea
RanaAshu-01 <rashu5733@gmail.com>
2026-04-06 19:08:48 +0530

    Nothing..

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,8 +6,6 @@ import CartPage from "../common_pages/Cart";
 import LoginPage from "../common_pages/LoginPage";
 import Deals from "../common_pages/Deals";
 import Account from "../common_pages/AccountPage";
-
-// mobile pages
 import Mobile from "../mobile_Pages/Mobile";
 import ElectronicItems from "../mobile_Pages/ElectronicItems";
 import Fashion from "../mobile_Pages/Fashion";
@@ -17,6 +15,23 @@ import Beauty from "../mobile_Pages/Beauty";
 import FoodHealth from "../mobile_Pages/Food&Health";
 import Furniture from "../mobile_Pages/Furniture";
 import SignUpPage from "../common_pages/SignUpPage";
+import OrderPlacedPage from "../common_pages/OrderPlaced";
+import Licence from "../common_pages/Licence";
+import CookiePolicy from "../common_pages/CookiePolicy"
+import Blogs from "../common_pages/Blogs"
+import Press from "../common_pages/PressPage";
+import Career from "../common_pages/Career"
+import AboutUs from "../common_pages/About";
+import ContactUs from "../common_pages/ContactUs"
+import HelpCenter from "../common_pages/Help";
+import SafetyCenter from "../common_pages/Safety"
+import CommunityGuidelines from "../common_pages/CommunityGuidelines";
+import Returns from "../common_pages/Returns";
+import Shipping from "../common_pages/Shipping";
+import OrderTracking from "../common_pages/OrderTracking"
+import GiftCards from "../common_pages/GiftCards"
+import PrivacyPolicy from "../common_pages/PrivacyPolicy"
+import TermsOfService  from "../common_pages/TermsOfService"
 
 const AppRoutes = () => {
   return (
@@ -29,6 +44,23 @@ const AppRoutes = () => {
         <Route path="/deals" element={<Deals />} />
         <Route path="/accounts" element={<Account />} />
         <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/order-placed" element={<OrderPlacedPage />} />
+        <Route path="/licenses" element={<Licence />} />
+        <Route path="/cookie-policy" element={<CookiePolicy />} />
+        <Route path="/blog" element={<Blogs />} />
+        <Route path="/press" element={<Press />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/help" element={<HelpCenter />} />
+        <Route path="/safety" element={<SafetyCenter />} />
+        <Route path="/community" element={<CommunityGuidelines />} />
+        <Route path="/returns" element={<Returns />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/order-tracking" element={<OrderTracking />} />
+        <Route path="/gift-cards" element={<GiftCards />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
         
 
         <Route path="/mobile" element={<Mobile />} />
